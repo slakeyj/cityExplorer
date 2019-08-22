85bba6242f040c50bf4f3e77c3bc472d6708c510
Susanna Lakey <susannalakey@gmail.com>
2019-08-22 13:44:47 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,42 +1,96 @@
 # cityExplorer
-City Explorer Lab 06 - Peter Carmichael
+## City Explorer
+
+# lab-08-back-end**Author**: Susanna Lakey
+**Version**: 1.0.0 (increment the patch/fix version number if you make more commits past your first submission)
+
+## Overview
+This is backend for a frontend application designed to allow users to access weather for the location that is asked for. This backend accesses JSON data stored locally to be supplied to the frontend in a readable format
+
+## Getting Started
+### If you build from scratch, you will have to install the following packages: 
+1. npm init -y
+2. npm install -S express dotenv
+3. npm install -S cors
+4. npm install -S superagent (for working with APIs)
+
+### If you copy the code down to your machine from my repo, simply:
+1. type npm install (to install all packages needed)
+2. Do a touch .env to create a file, then within that .env file add:
+* add PORT=port_number
+* Add an environment variable to your server named `EVENTBRITE_API_KEY `,
+* Add an environment variable to your server named `WEATHER_API_KEY`, and use it appropriately in your code. 
+* Add an environment variable to your server named `GEOCODE_API_KEY `, and use it appropriately in your code.
+
+### Front End app exists here:
+https://codefellows.github.io/code-301-guide/curriculum/city-explorer-app/front-end/
+
+### To start server:
+Type into terminal: <nodemon>, then, to restart at any time, enter `rs`
+
+## Architecture
+This application was designed with
+* JavaScript
+* API Calls 
+<!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
+
+
+## Change Log
+
+08-20-2019 9:15am - Put together file structure.
+
+08-20-2019 10:00am - Created object literal to pull data from geo.json
+
+08-20-2019 10:15am - Made object literal into a constructor function and created a new instance of the location data which is appearing and the route is responding in the deployed static client.
+
+08-20-2019 10:35am - Deployed to Heroku.
+
+08-20-2019 1:50pm - Weather data time is now converted from milliseconds into a date.
+
+08-21-2019 9:40am - File structure in place for new driver's computer
+
+08-21-2019 10:20am - Completed code review of yesterday's code
+
+08-21-2019 10:28am - Deployed to Heroku
+
+08-21-2019 11:28am - locations functionality completed using API
+
+08-21-2019 11:50am - Weather functionality completed using API
+
+08-21-2019 4pm - Events functionality in place using API
+
+08-22-2019 9:20am - Database Created
+
+08-22-2019 9:45am - Google map data stored in SQL database table.
+
+08-22-2019 10:50am - Dark Sky data stored in SQL database table.
+
+08-22-2019 11:45am - Eventbrite data stored in SQL database table.
 
 
 
-<!-- FEATURES -->
 
-<!-- /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\ -->
 
-Number and name of feature: Create File Tree and Make server live
+## Credits and Collaborations
+* Peter Charmichaael
 
-Estimate of time needed to complete: 60 min
+# Features
+## Number and name of feature: Tuesday Lab
 
-Start time: _9:00____
+Estimate of time needed to complete: 4 hours
 
-Finish time: 10:06
+Start time: 9am
 
-Actual time needed to complete: 66:00
+Finish time: 2pm, minus 1 hour, plus another 45 min
 
-<!-- /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\ -->
+Actual time needed to complete: 4 hours 45 min
 
-Number and name of feature: Constructor Function
+## Number and name of feature: Wednesday Feature - Adding API functionality
 
-Estimate of time needed to complete: 60:00
+Estimate of time needed to complete: 3 hours
 
-Start time: 10:10
+Start time: 9am
 
-Finish time: _____
+Finish time: 2pm, minus 1 hour, plus another 45 min
 
-Actual time needed to complete: _____
-
-<!-- /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\ -->
-
-Number and name of feature: ________________________________
-
-Estimate of time needed to complete: _____
-
-Start time: _____
-
-Finish time: _____
-
-Actual time needed to complete: _____
+Actual time needed to complete: 4 hours 45 min
--- server.js
+++ server.js
@@ -19,146 +19,146 @@ client.on('error', (error) => console.error(error));
 const PORT = process.env.PORT;
 
 function Location(query, format, lat, lng) {
-    this.search_query = query;
-    this.formatted_query = format;
-    this.latitude = lat;
-    this.longitude = lng;
+  this.search_query = query;
+  this.formatted_query = format;
+  this.latitude = lat;
+  this.longitude = lng;
 }
 
 app.get('/location', (request, response) => {
-    const query = request.query.data; //seattle
-    // console.log('LOCATION QUERY', query);
-    client.query(`SELECT * FROM locations WHERE search_query=$1`, [query]).then(sqlResult => {
-        if (sqlResult.rowCount > 0) {
-            response.send(sqlResult.rows[0]);
-        } else {
+  const query = request.query.data; //seattle
+  // console.log('LOCATION QUERY', query);
+  client.query(`SELECT * FROM locations WHERE search_query=$1`, [query]).then(sqlResult => {
+    if (sqlResult.rowCount > 0) {
+      response.send(sqlResult.rows[0]);
+    } else {
 
 
-            const urlToVisit = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
+      const urlToVisit = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
 
-            // superagent.get('url as a string');
-            superagent.get(urlToVisit).then(responseFromSuper => {
-                // console.log('stuff', responseFromSuper.body);
+      // superagent.get('url as a string');
+      superagent.get(urlToVisit).then(responseFromSuper => {
+        // console.log('stuff', responseFromSuper.body);
 
-                // I simply replaced my geodata require, with the data in the body of my superagent response
-                const geoData = responseFromSuper.body;
+        // I simply replaced my geodata require, with the data in the body of my superagent response
+        const geoData = responseFromSuper.body;
 
-                //THIS IS WHAT WRITES DATA TO SQL DATABASE
+        //THIS IS WHAT WRITES DATA TO SQL DATABASE
 
-                const specificGeoData = geoData.results[0];
+        const specificGeoData = geoData.results[0];
 
-                const formatted = specificGeoData.formatted_address;
-                const lat = specificGeoData.geometry.location.lat;
-                const lng = specificGeoData.geometry.location.lng;
+        const formatted = specificGeoData.formatted_address;
+        const lat = specificGeoData.geometry.location.lat;
+        const lng = specificGeoData.geometry.location.lng;
 
-                const newLocation = new Location(query, formatted, lat, lng)
+        const newLocation = new Location(query, formatted, lat, lng)
 
-                const sqlQueryInsert = `INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1,$2,$3,$4);`;
-                const sqlValueArr = [newLocation.search_query, newLocation.formatted_query, newLocation.latitude, newLocation.longitude];
-                client.query(sqlQueryInsert, sqlValueArr);
+        const sqlQueryInsert = `INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1,$2,$3,$4);`;
+        const sqlValueArr = [newLocation.search_query, newLocation.formatted_query, newLocation.latitude, newLocation.longitude];
+        client.query(sqlQueryInsert, sqlValueArr);
 
-                response.send(newLocation);
-            }).catch(error => {
-                response.status(500).send(error.message);
-                console.error(error);
-            })
-        }
+        response.send(newLocation);
+      }).catch(error => {
+        response.status(500).send(error.message);
+        console.error(error);
+      })
+    }
 
-    })
+  })
 })
 
 
 function Day(summary, time) {
-    this.forecast = summary;
-    this.time = new Date(time * 1000).toDateString();
+  this.forecast = summary;
+  this.time = new Date(time * 1000).toDateString();
 }
 
 app.get('/weather', (request, response) => {
-    // console.log(request);
+  // console.log(request);
 
-    let localData = request.query.data;
-    console.log('LOCAL DATA', localData);
+  let localData = request.query.data;
+  console.log('LOCAL DATA', localData);
 
-    client.query(`SELECT * FROM weather WHERE search_query=$1`, [localData.search_query]).then(sqlResult => {
-        if (sqlResult.rowCount > 0) {
-            response.send(sqlResult.rows[0]);
-        } else {
+  client.query(`SELECT * FROM weather WHERE search_query=$1`, [localData.search_query]).then(sqlResult => {
+    if (sqlResult.rowCount > 0) {
+      response.send(sqlResult.rows[0]);
+    } else {
 
-            const darkSkyUrl = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${localData.latitude},${localData.longitude}`;
+      const darkSkyUrl = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${localData.latitude},${localData.longitude}`;
 
-            console.log(darkSkyUrl);
+      console.log(darkSkyUrl);
 
-            superagent.get(darkSkyUrl).then(responseFromSuper => {
-                // console.log('Location Body', responseFromSuper.body);
+      superagent.get(darkSkyUrl).then(responseFromSuper => {
+        // console.log('Location Body', responseFromSuper.body);
 
-                const weatherBody = responseFromSuper.body;
+        const weatherBody = responseFromSuper.body;
 
-                const eightDays = weatherBody.daily.data;
-                // console.log('DAILY DATA', eightDays);
-                console.log('8 DAYS', eightDays);
-                const formattedDays = eightDays.map(day =>
-                    new Day(day.summary, day.time));
+        const eightDays = weatherBody.daily.data;
+        // console.log('DAILY DATA', eightDays);
+        console.log('8 DAYS', eightDays);
+        const formattedDays = eightDays.map(day =>
+          new Day(day.summary, day.time));
 
-                console.log('formatted days', formattedDays);
-                formattedDays.forEach(day => {
-                    const sqlQueryInsert = `INSERT INTO weather (search_query, forecast, time) VALUES ($1,$2,$3);`;
-                    const sqlValueArr = [localData.search_query, day.forecast, day.time];
-                    client.query(sqlQueryInsert, sqlValueArr);
-                })
-                response.send(formattedDays);
-            }).catch(error => {
-                response.status(500).send(error.message);
-                console.error(error);
-            })
+        console.log('formatted days', formattedDays);
+        formattedDays.forEach(day => {
+          const sqlQueryInsert = `INSERT INTO weather (search_query, forecast, time) VALUES ($1,$2,$3);`;
+          const sqlValueArr = [localData.search_query, day.forecast, day.time];
+          client.query(sqlQueryInsert, sqlValueArr);
+        })
+        response.send(formattedDays);
+      }).catch(error => {
+        response.status(500).send(error.message);
+        console.error(error);
+      })
 
-        }
+    }
 
-    })
+  })
 
 })
 
 // constructor function for eventbrite
 function Events(link, name, date, summary) {
-    this.link = link;
-    this.name = name;
-    this.event_date = new Date(date).toDateString();
-    this.summary = summary;
+  this.link = link;
+  this.name = name;
+  this.event_date = new Date(date).toDateString();
+  this.summary = summary;
 }
 // set up an app.get for /eventbrite
 app.get('/events', (request, response) => {
-    let eventData = request.query.data;
-    console.log('event data', eventData);
-    client.query(`SELECT * FROM events WHERE search_query=$1`, [eventData.search_query]).then(sqlResult => {
-        if (sqlResult.rowCount > 0) {
-            response.send(sqlResult.rows[0]);
-        } else {
+  let eventData = request.query.data;
+  console.log('event data', eventData);
+  client.query(`SELECT * FROM events WHERE search_query=$1`, [eventData.search_query]).then(sqlResult => {
+    if (sqlResult.rowCount > 0) {
+      response.send(sqlResult.rows[0]);
+    } else {
 
-            const eventUrlData =
-                `https://www.eventbriteapi.com/v3/events/search/?sort_by=date&location.latitude=${eventData.latitude}&location.longitude=${eventData.longitude}&token=ZDDD2HU3AK5DLAZ6IYF5`
+      const eventUrlData =
+        `https://www.eventbriteapi.com/v3/events/search/?sort_by=date&location.latitude=${eventData.latitude}&location.longitude=${eventData.longitude}&token=ZDDD2HU3AK5DLAZ6IYF5`
 
 
-            superagent.get(eventUrlData).then(responseFromSuper => {
-                // console.log('stuff', responseFromSuper.body.events);
+      superagent.get(eventUrlData).then(responseFromSuper => {
+        // console.log('stuff', responseFromSuper.body.events);
 
-                const eventBody = responseFromSuper.body.events;
+        const eventBody = responseFromSuper.body.events;
 
-                const dailyEvents = eventBody.map(day => new Events(day.url, day.name.text, day.start.local, day.description.text));
+        const dailyEvents = eventBody.map(day => new Events(day.url, day.name.text, day.start.local, day.description.text));
 
-                dailyEvents.forEach(event => {
-                    const sqlQueryInsert = `INSERT INTO events (search_query, link, name, event_date, summary) VALUES ($1,$2,$3,$4,$5);`;
-                    const sqlValueArr = [eventData.search_query, event.link, event.name, event.date, event.summary];
-                    client.query(sqlQueryInsert, sqlValueArr);
-                })
+        dailyEvents.forEach(event => {
+          const sqlQueryInsert = `INSERT INTO events (search_query, link, name, event_date, summary) VALUES ($1,$2,$3,$4,$5);`;
+          const sqlValueArr = [eventData.search_query, event.link, event.name, event.date, event.summary];
+          client.query(sqlQueryInsert, sqlValueArr);
+        })
 
-                response.send(dailyEvents);
-            }).catch(error => {
-                response.status(500).send(error.message);
-                console.error(error);
+        response.send(dailyEvents);
+      }).catch(error => {
+        response.status(500).send(error.message);
+        console.error(error);
 
-            })
+      })
 
-        }
-    })
+    }
+  })
 })
 
 
